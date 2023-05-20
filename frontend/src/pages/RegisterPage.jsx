58ec71351e988ce7d81174cca7ad3a5da8c1cf54
Alex Xu <xulex6@gmail.com>
2023-05-20 22:22:21 +1000

    feat: add register page

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,225 @@
+import { Box, Button, TextField, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const USERNAME_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{2,23}$/;
+const PASSWORD_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,20}$/;
+
+const RegisterPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/';
+
+  // const [errorMessage, setErrorMessage] = useState('');
+
+  const [email, setEmail] = useState('');
+  const [isValidEmail, setValidEmail] = useState(false);
+  const [isEmailFocus, setEmailFocus] = useState(false);
+
+  const [username, setUserName] = useState('');
+  const [isValidName, setValidName] = useState(false);
+  const [isNameFocus, setNameFocus] = useState(false);
+
+  const [password, setPassword] = useState('');
+  const [isValidPassword, setValidPassword] = useState(false);
+  const [isPasswordFocus, setPasswordFocus] = useState(false);
+
+  const [passwordMatch, setPasswordMatch] = useState('');
+  const [isValidMatch, setValidMatch] = useState(false);
+  const [isMatchFocus, setMatchFocus] = useState(false);
+
+  // useEffect(() => {
+  //   setErrorMessage('');
+  // }, [email, password, passwordMatch]);
+
+  useEffect(() => {
+    const result = EMAIL_REGEX.test(email);
+    setValidEmail(result);
+  }, [email]);
+
+  useEffect(() => {
+    const result = USERNAME_REGEX.test(username);
+    setValidName(result);
+  }, [username]);
+
+  useEffect(() => {
+    const result = PASSWORD_REGEX.test(password);
+    setValidPassword(result);
+    setValidMatch(password === passwordMatch);
+  }, [password, passwordMatch]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5005/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          name: username,
+          password,
+        }),
+      });
+      const data = await response.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      const token = data?.token;
+      localStorage.setItem('token', token);
+      navigate(from, { replace: true });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        height: '90vh',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Typography variant="h2" sx={{ mb: 3 }}>
+        Register for BigBrain
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          maxWidth: 800,
+          borderRadius: 2,
+          boxShadow: 5,
+          rowGap: 2,
+          p: 5,
+        }}
+      >
+        <TextField
+          id="register-email"
+          type="email"
+          name="register-email"
+          label="Email"
+          placeholder="example@email.com"
+          fullWidth
+          required
+          autoFocus
+          variant="standard"
+          error={isEmailFocus && !isValidEmail}
+          helperText={
+            isEmailFocus && !isValidEmail && '⚠️ Please enter a valid email.'
+          }
+          onChange={(event) => setEmail(event.target.value)}
+          onFocus={() => setEmailFocus(true)}
+          onBlur={() => setEmailFocus(false)}
+        />
+        <TextField
+          id="register-name"
+          name="register-name"
+          label="Username"
+          placeholder="Your Username"
+          fullWidth
+          required
+          variant="standard"
+          error={isNameFocus && !isValidName}
+          helperText={
+            isNameFocus &&
+            !isValidName &&
+            '⚠️ Please enter a single name, at least 3 characters without spaces.'
+          }
+          onChange={(event) => setUserName(event.target.value)}
+          onFocus={() => setNameFocus(true)}
+          onBlur={() => setNameFocus(false)}
+        />
+        <TextField
+          id="register-password"
+          type="password"
+          name="register-password"
+          label="Password"
+          placeholder="••••••••••"
+          fullWidth
+          required
+          variant="standard"
+          error={isPasswordFocus && !isValidPassword}
+          helperText={
+            isPasswordFocus &&
+            !isValidPassword && (
+              <span>
+                Your password must be between <b>8 to 20 characters</b> contain
+                at least:
+                <li>An uppercase letter</li>
+                <li>A lowercase letter</li>
+                <li>One number</li>
+                <li>A special character</li>
+              </span>
+            )
+          }
+          onChange={(event) => setPassword(event.target.value)}
+          onFocus={() => setPasswordFocus(true)}
+          onBlur={() => setPasswordFocus(false)}
+        />
+        <TextField
+          type="password"
+          name="register-password-confirm"
+          id="register-password-confirm"
+          label="Retype Password"
+          placeholder="••••••••••"
+          fullWidth
+          required
+          variant="standard"
+          error={isMatchFocus && !isValidMatch}
+          helperText={
+            isMatchFocus && !isValidMatch && '⚠️ Your passwords must match.'
+          }
+          onChange={(event) => setPasswordMatch(event.target.value)}
+          onFocus={() => setMatchFocus(true)}
+          onBlur={() => setMatchFocus(false)}
+        />
+
+        <Typography variant="body2" sx={{ fontSize: 16 }}>
+          {'Already have an account? '}
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            component="a"
+            sx={{
+              cursor: 'pointer',
+              textDecoration: 'underline',
+              fontSize: 16,
+            }}
+            onClick={() => navigate('/login')}
+          >
+            Login here
+          </Typography>
+        </Typography>
+
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          disabled={
+            !!(
+              !isValidPassword ||
+              !isValidMatch ||
+              !isValidEmail ||
+              !isValidName
+            )
+          }
+        >
+          Sign up
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+export default RegisterPage;
