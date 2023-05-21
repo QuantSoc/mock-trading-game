0571fa64c3b10e4a9b7400b8e5b85b514a7a00bf
Alex Xu <xulex6@gmail.com>
2023-05-21 17:18:46 +1000

    feat: add create game button to dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,46 @@
+import { useState, useEffect } from 'react';
+import { fetchAPIRequest } from '../helpers';
+import { RedirectButton } from '../components/index.js';
+import { Box } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [userGames, setUserGames] = useState({});
+
+  const fetchGames = async () => {
+    await fetchAPIRequest('/games', 'GET');
+    const gameData = await fetchAPIRequest('/games', 'GET');
+    return gameData;
+  };
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      setIsLoading(true);
+      await fetchAPIRequest('/games', 'GET');
+      const gameData = await fetchAPIRequest('/games', 'GET');
+      setUserGames(gameData);
+      setIsLoading(false);
+    };
+
+    fetchGames();
+  }, []);
+
+  useEffect(() => {
+    console.log(userGames);
+  }, [userGames]);
+
+  return (
+    <Box>
+      <RedirectButton
+        destination="/game/new"
+        btnText="New Game"
+        variant="contained"
+        color="primary"
+        isStartIcon={true}
+        icon={<AddIcon />}
+      />
+    </Box>
+  );
 };
 export default Dashboard;
