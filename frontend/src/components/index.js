12a55c60ce9824ee511c643f9ac9d30fdb59f140
Alex Xu <xulex6@gmail.com>
2023-05-26 23:33:42 +1000

    feat: import modal, create game button, game card

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,5 @@
 export { default as ProtectedRoute } from './ProtectedRoute.jsx';
 export { default as RedirectButton } from './RedirectButton.jsx';
+export { default as Modal } from './Modal.jsx';
+export { default as CreateGameButton } from './CreateGameButton.jsx';
+export { default as GameCard } from './GameCard.jsx';
