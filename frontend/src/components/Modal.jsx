751b223c661898f39c1db8502856b91ec58903e4
Alex Xu <xulex6@gmail.com>
2023-05-26 23:32:47 +1000

    feat: add modal component called by modal hook

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,45 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Modal = ({
+  isModalShown,
+  toggleModal,
+  modalTitle,
+  children,
+  actions,
+}) => {
+  return ReactDOM.createPortal(
+    <React.Fragment>
+      <Dialog
+        open={isModalShown}
+        onClose={toggleModal}
+        aria-labelledby="modal-dialog-title"
+        aria-describedby="modal-dialog-description"
+        fullWidth={true}
+        maxWidth="sm"
+      >
+        <DialogTitle id="modal-dialog-title">
+          <Typography sx={{ fontSize: 32 }}>{modalTitle}</Typography>
+        </DialogTitle>
+        <DialogContent>{children}</DialogContent>
+        <DialogActions>
+          {actions}
+          <Button variant="contained" onClick={toggleModal} margin="dense">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>,
+    document.body
+  );
+};
+
+export default Modal;
