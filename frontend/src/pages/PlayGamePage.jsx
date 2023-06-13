96c08d56dcc8aeaa398392534b48f6d47a8d7eaf
Alex Xu <xulex6@gmail.com>
2023-06-13 23:28:17 +1000

    feat: add team trade input and game progression

--- frontend/src/pages/PlayGamePage.jsx
+++ frontend/src/pages/PlayGamePage.jsx
@@ -1,41 +1,225 @@
 import { useParams } from 'react-router-dom';
 import { fetchAPIRequest } from '../helpers';
-import { useState } from 'react';
-import { Button, TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Divider,
+  TextField,
+  Typography,
+  Grid,
+} from '@mui/material';
+import PriceChangeIcon from '@mui/icons-material/PriceChange';
+import { BidAskPanel, TradePanel } from '../components/index.js';
 
 const PlayGamePage = () => {
   const { sessionId } = useParams();
   const [teamName, setTeamName] = useState('');
-  const [bid, setBid] = useState('');
-  const [ask, setAsk] = useState('');
-  const [teamId, setTeamId] = useState('');
+  const [myTeamId, setMyTeamId] = useState('');
+
+  const [position, setPosition] = useState(-1);
+  const [questions, setQuestions] = useState([]);
+  const [teams, setTeams] = useState({});
+  const [marketPosition, setMarketPosition] = useState(0);
 
   const createTeam = async () => {
     const teamData = await fetchAPIRequest(`/game/join/${sessionId}`, 'POST', {
       name: teamName,
     });
-    console.log(teamData);
-    setTeamId(teamData.teamId);
-  };
-  const submitSpread = async () => {
-    const teamData = await fetchAPIRequest(`/game/${teamId}/submit`, 'PUT', {
-      bid,
-      ask,
-    });
-  };
-  const advanceGame = async () => {
-    await fetchAPIRequest(`/games/641196079/next`, 'POST');
+    setMyTeamId(teamData.teamId);
   };
 
+  const processResults = (teams) => {
+    const teamResults = Object.keys(teams)
+      .map((teamId, index) => {
+        const trueValue = questions[position].trueValue;
+        const balance = teams[teamId].teamAnswers[marketPosition].balance;
+        const contracts = teams[teamId].teamAnswers[marketPosition].contracts;
+        const total =
+          parseInt(contracts, 10) * parseFloat(trueValue, 10) +
+          parseFloat(balance, 10);
+        return {
+          teamId,
+          teamName: teams[teamId].name,
+          balance,
+          contracts,
+          total,
+        };
+      })
+      .sort((a, b) => (a.total < b.total ? 1 : -1));
+
+    const winningTotal = teamResults[0]?.total;
+    return teamResults.map((result) => {
+      return { ...result, isWinner: result.total === winningTotal };
+    });
+  };
+
+  useEffect(() => {
+    const getGameStatus = async () => {
+      const status = await fetchAPIRequest(
+        `/session/${sessionId}/status`,
+        'GET'
+      );
+      setPosition(status.position);
+      setQuestions(status.questions);
+      setTeams(status.teams);
+      if (
+        status.position >= 0 &&
+        status.questions[status.position].type === 'market'
+      ) {
+        setMarketPosition(status.position);
+      }
+    };
+    setInterval(() => {
+      getGameStatus();
+    }, 1000);
+  }, [sessionId]);
+
   return (
-    <div>
-      <TextField onChange={(event) => setTeamName(event.target.value)} />
-      <Button onClick={createTeam}>Create Team</Button>
-      <TextField label="bid" onChange={(event) => setBid(event.target.value)} />
-      <TextField label="ask" onChange={(event) => setAsk(event.target.value)} />
-      <Button onClick={submitSpread}>Submit Bid Ask Spread</Button>
-      <Button onClick={advanceGame}>Advance</Button>
-    </div>
+    <Box
+      sx={{
+        width: '100%',
+        minHeight: '92.5vh',
+        flex: '1 1 auto',
+        height: 'fit-content',
+        boxSizing: 'border-box',
+        display: 'flex',
+        justifyContent: 'center',
+        pt: 10,
+        px: { xs: 1, sm: 10, md: 18, lg: 25 },
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: '#fff',
+          flexGrow: 1,
+          borderRadius: '20px 20px 0px 0px',
+          boxShadow: 3,
+          width: '100%',
+          px: { xs: 2, sm: 5 },
+          py: 7,
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            whiteSpace: 'nowrap',
+            mb: 5,
+          }}
+        >
+          <PriceChangeIcon sx={{ width: 50, height: 50, mr: 1 }} />
+          Mock Trading
+        </Typography>
+        <Box>
+          {position < 0 ? (
+            <>
+              <Typography
+                variant="h5"
+                sx={{ display: 'flex', alignItems: 'center', my: 2 }}
+              >
+                <CircularProgress sx={{ mr: 2 }} />
+                Waiting for session to start
+              </Typography>
+              <Typography autoFocus variant="h5" sx={{ mt: 3 }}>
+                Set a team name:
+              </Typography>
+              <TextField
+                label="Team Name"
+                onChange={(event) => setTeamName(event.target.value)}
+              />
+              <Button onClick={createTeam}>Create Team</Button>
+            </>
+          ) : !myTeamId ? (
+            <Typography variant="h4" color="text.secondary">
+              Session already started...
+            </Typography>
+          ) : (
+            <>
+              <Box
+                sx={{
+                  boxShadow: 3,
+                  borderRadius: 5,
+                  width: { xs: '80%', md: '70%' },
+                  height: 'fit-content',
+                  py: 4,
+                  px: 4,
+                  mx: 'auto',
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  color="text.secondary"
+                  sx={{ float: 'right' }}
+                >
+                  {position.toString()}
+                </Typography>
+                <Typography variant="h4">
+                  {questions[position]?.type[0].toUpperCase() +
+                    questions[position]?.type.slice(1)}
+                </Typography>
+                <Typography variant="h6" color="text.secondary">
+                  {questions[position]?.name}
+                </Typography>
+                <Typography variant="h6">
+                  {questions[position]?.hint}
+                </Typography>
+                <Divider sx={{ my: 3 }} />
+                <Typography variant="h6">
+                  {questions[position].type === 'result' &&
+                    `The fair value of this market is $${questions[position].trueValue}.`}
+                </Typography>
+                <Typography variant="h6">
+                  {questions[position].type !== 'round' &&
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis officia odit nulla aliquid consequuntur id unde doloribus impedit quia tempora debitis ut et labore ex hic, officiis, sequi magni odio.'}
+                </Typography>
+              </Box>
+              {questions[position].type === 'round' && (
+                <BidAskPanel
+                  teamId={myTeamId}
+                  teamName={teams[myTeamId]?.name}
+                  balance={teams[myTeamId]?.teamAnswers[marketPosition].balance}
+                  contracts={
+                    teams[myTeamId]?.teamAnswers[marketPosition].contracts
+                  }
+                />
+              )}
+              <Grid
+                container
+                columns={{ xs: 2, sm: 8, md: 12, lg: 16 }}
+                spacing={3}
+                sx={{ py: 3 }}
+              >
+                {questions[position].type === 'result' &&
+                  processResults(teams).map((team, index) => {
+                    return (
+                      <Grid
+                        item
+                        xs={2}
+                        sm={4}
+                        md={4}
+                        key={index}
+                        sx={{ display: 'flex', justifyContent: 'center' }}
+                      >
+                        <TradePanel
+                          key={team.teamId}
+                          teamName={team.teamName}
+                          balance={team.balance}
+                          contracts={team.contracts}
+                          total={team.total}
+                          isWinner={team.isWinner}
+                        />
+                      </Grid>
+                    );
+                  })}
+              </Grid>
+            </>
+          )}
+        </Box>
+      </Box>
+    </Box>
   );
 };
 export default PlayGamePage;
