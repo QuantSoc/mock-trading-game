11213dcc5975fb8582ad38832e874c5f43b8d7cf
Alex Xu <xulex6@gmail.com>
2023-06-13 23:25:56 +1000

    feat: update team panel to truncate overflow

--- frontend/src/components/TeamPanel.jsx
+++ frontend/src/components/TeamPanel.jsx
@@ -8,9 +8,19 @@ const TeamPanel = ({ teamName, balance, contracts, latestBid, latestAsk }) => {
         flexDirection: 'column',
         borderTop: '0.5px solid #ccc',
         borderBottom: '0.5px solid #ccc',
+        p: 2,
       }}
     >
-      <Typography variant="h5">{teamName}</Typography>
+      <Typography
+        variant="h5"
+        sx={{
+          whiteSpace: 'nowrap',
+          textOverflow: 'ellipsis',
+          overflow: 'hidden',
+        }}
+      >
+        {teamName}
+      </Typography>
       <Typography variant="h6">Balance: ${balance}</Typography>
       <Typography variant="h6">Contracts: {contracts}</Typography>
       <Typography variant="h6">Bid: ${latestBid}</Typography>
