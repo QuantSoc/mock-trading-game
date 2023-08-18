ca27c82997a18c572be250981cb7867fe981a88e
Alex Xu <xulex6@gmail.com>
2023-08-18 10:33:42 +0000

    feat: add results page for session results

---
+++ frontend/src/pages/GameHistoryPage/QuestionAccordion.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { useState } from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {
+  Box,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+  Divider,
+  Grid,
+} from '@mui/material';
+import TeamStats from './TeamStats';
+
+const QuestionAccordion = ({
+  qIndex,
+  marketName,
+  round,
+  type,
+  teams,
+  trueValue,
+}) => {
+  const [expanded, setExpanded] = useState(false);
+  const processResults = (teams) => {
+    const teamResults = Object.keys(teams)
+      .map((teamId, index) => {
+        const balance = teams[teamId].teamAnswers[0].balance;
+        const contracts = teams[teamId].teamAnswers[0].contracts;
+        const total =
+          parseInt(contracts, 10) * parseFloat(trueValue, 10) +
+          parseFloat(balance, 10);
+        return {
+          teamId,
+          teamName: teams[teamId].name,
+          balance,
+          contracts,
+          total,
+        };
+      })
+      .sort((a, b) => (a.total < b.total ? 1 : -1));
+
+    const winningTotal = teamResults[0]?.total;
+    return winningTotal;
+  };
+
+  return (
+    <Box sx={{ mb: 2 }}>
+      <Accordion
+        expanded={expanded}
+        onClick={() => setExpanded((prev) => !prev)}
+        sx={{
+          border: '0.5px solid',
+          borderColor: type === 'Result' ? 'gold' : '#EAEAEA',
+        }}
+      >
+        <AccordionSummary expandIcon={!marketName && <ExpandMoreIcon />}>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-between',
+              pr: 1,
+            }}
+          >
+            <Typography>{type}</Typography>
+            <Typography sx={{ color: 'text.secondary' }}>
+              {!!round && round}
+              {!!marketName && marketName}
+            </Typography>
+          </Box>
+        </AccordionSummary>
+        {!marketName && <Divider sx={{ my: 1 }} />}
+        {!marketName && (
+          <AccordionDetails onClick={(e) => e.stopPropagation()}>
+            <Grid
+              container
+              columnSpacing={5}
+              rowSpacing={5}
+              columns={12}
+              sx={{ gridAutoFlow: 'column' }}
+            >
+              {!!teams &&
+                Object.keys(teams).map((teamId, index) => {
+                  const curr = teams[teamId].teamAnswers;
+                  return (
+                    <Grid
+                      key={`team-stats-grid-${index}`}
+                      item
+                      xs={12}
+                      md={6}
+                      xl={4}
+                    >
+                      <TeamStats
+                        key={`team-stats-${index}`}
+                        teamName={teams[teamId].name}
+                        bid={curr[qIndex]?.bid}
+                        ask={curr[qIndex]?.ask}
+                        balance={curr[qIndex].balance}
+                        contracts={curr[qIndex]?.contracts}
+                        isWinner={curr[qIndex]?.isWinner}
+                        trueValue={trueValue}
+                      />
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </AccordionDetails>
+        )}
+      </Accordion>
+      {type === 'Result' && <Divider sx={{ my: 4 }} />}
+    </Box>
+  );
+};
+
+export default QuestionAccordion;
---
+++ frontend/src/pages/GameHistoryPage/TeamStats.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Box, Typography, Divider } from '@mui/material';
+import GroupIcon from '@mui/icons-material/Group';
+import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
+import RequestPageOutlinedIcon from '@mui/icons-material/RequestPageOutlined';
+import ArrowDropUpOutlinedIcon from '@mui/icons-material/ArrowDropUpOutlined';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import PriceChangeOutlinedIcon from '@mui/icons-material/PriceChangeOutlined';
+
+const TeamStats = ({
+  teamName,
+  bid,
+  ask,
+  balance,
+  contracts,
+  trueValue,
+  isWinner,
+}) => {
+  return (
+    <Box
+      sx={{
+        borderRadius: 1,
+        p: 2,
+        border: '0.5px solid',
+        borderColor: isWinner ? 'limegreen' : '#EAEAEA',
+      }}
+    >
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography
+          fontWeight={500}
+          sx={{
+            whiteSpace: 'nowrap',
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <GroupIcon fontSize="small" sx={{ mr: 1 }} />
+          {teamName}
+        </Typography>
+        <EmojiEventsIcon
+          // fontSize="large"
+          sx={{
+            color: 'gold',
+            mb: 1,
+            visibility: isWinner ? 'visible' : 'hidden',
+          }}
+        />
+      </Box>
+      <Divider sx={{ my: 2 }} />
+      <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
+        <Typography sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+          <PaidOutlinedIcon sx={{ mr: 1 }} />
+          {balance?.toFixed(2)}
+        </Typography>
+        <Typography
+          variant="h6"
+          fontSize={16}
+          sx={{ display: 'flex', alignItems: 'center', mb: 2 }}
+        >
+          <RequestPageOutlinedIcon color="primary" sx={{ mr: 1 }} />
+          {`${contracts} `}
+          <Typography color="text.secondary" sx={{ ml: 1 }}>
+            contracts
+          </Typography>
+        </Typography>
+      </Box>
+      {!trueValue && (
+        <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
+          <Typography sx={{ display: 'flex', alignItems: 'center' }}>
+            <ArrowDropDownOutlinedIcon color="error" fontSize="large" />
+            Bid {bid ? bid.toFixed(2) : '--'}
+          </Typography>
+          <Typography sx={{ display: 'flex', alignItems: 'center' }}>
+            <ArrowDropUpOutlinedIcon color="success" fontSize="large" />
+            Ask {ask ? ask.toFixed(2) : '--'}
+          </Typography>
+        </Box>
+      )}
+      {trueValue && (
+        <Typography
+          variant="h6"
+          fontSize={16}
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            mt: 2,
+          }}
+        >
+          <PriceChangeOutlinedIcon color="success" sx={{ mr: 1 }} />
+          <Typography color="text.secondary" sx={{ mr: 2 }}>
+            Total
+          </Typography>
+          {`$${trueValue * contracts - balance} `}
+        </Typography>
+      )}
+    </Box>
+  );
+};
+
+export default TeamStats;
