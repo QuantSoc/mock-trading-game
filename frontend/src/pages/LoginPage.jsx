c25e42614d0168f568758a47d720bc7bdf9dfb52
Alex Xu <xulex6@gmail.com>
2023-08-27 12:11:45 +0000

    feat: remove auth page images

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { TextField, Button, Box, Typography, Divider } from '@mui/material';
+import { TextField, Button, Box, Typography } from '@mui/material';
 import { ReactComponent as QuantsocIcon } from '../assets/quantsoc.svg';
 import { BACKEND_ROUTE } from '../constants';
 import { AlertContext } from '../contexts/NotificationContext';
@@ -45,97 +45,82 @@ const LoginPage = () => {
         height: '92.5vh',
         width: '100%',
         display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
       <Box
-        sx={{ width: { xs: '0%', lg: '50%' }, objectFit: 'cover' }}
-        component="img"
-        src="https://source.unsplash.com/random?trading,stocks,quant,finance,math"
-      />
-      <Box
         sx={{
-          width: { xs: '100%', lg: '50%' },
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+        }}
+      >
+        <QuantsocIcon style={{ maxWidth: 100, padding: '0 20px', mb: 20 }} />
+        <Typography sx={{ fontSize: 34 }}>Mock Trading Game</Typography>
+      </Box>
+      <Typography variant="h5" textAlign="center">
+        Sign In
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          width: '100%',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
-          justifyContent: 'center',
-          p: 3,
+          maxWidth: 800,
+          minWidth: 300,
+          rowGap: 7,
+          py: 2,
+          px: { xs: 7, sm: 20 },
+          m: 3,
           boxSizing: 'border-box',
         }}
       >
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexWrap: 'wrap',
-          }}
-        >
-          <QuantsocIcon
-            style={{ maxWidth: 125, padding: '0 20px', marginBottom: '10px' }}
-          />
-          <Typography variant="h3">Competitions</Typography>
-        </Box>
-        <Divider sx={{ py: 1 }} />
-        <Typography variant="h4">Sign In</Typography>
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          sx={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            maxWidth: 800,
-            minWidth: 300,
-            rowGap: 7,
-            py: 5,
-            px: 10,
-            m: 3,
-            boxSizing: 'border-box',
-          }}
-        >
-          <TextField
-            id="login-email"
-            label="Email"
-            variant="standard"
-            type="email"
-            name="login-email"
-            onChange={(event) => setEmail(event.target.value)}
-            placeholder="example@email.com"
-            fullWidth
-            autoFocus
-          />
-          <TextField
-            id="login-password"
-            label="Password"
-            variant="standard"
-            type="password"
-            name="register-password"
-            placeholder="••••••••••"
-            fullWidth
-            onChange={(event) => setPassword(event.target.value)}
-          />
-          <Typography variant="body2" sx={{ fontSize: 16 }}>
-            {"Don't have an account? "}
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              component="a"
-              sx={{
-                cursor: 'pointer',
-                textDecoration: 'underline',
-                fontSize: 16,
-              }}
-              onClick={() => navigate('/register')}
-            >
-              Sign up here
-            </Typography>
+        <TextField
+          id="login-email"
+          label="Email"
+          variant="standard"
+          type="email"
+          name="login-email"
+          onChange={(event) => setEmail(event.target.value)}
+          placeholder="example@email.com"
+          fullWidth
+          autoFocus
+        />
+        <TextField
+          id="login-password"
+          label="Password"
+          variant="standard"
+          type="password"
+          name="register-password"
+          placeholder="••••••••••"
+          fullWidth
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <Typography variant="body2" sx={{ fontSize: 16 }}>
+          {"Don't have an account? "}
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            component="a"
+            sx={{
+              cursor: 'pointer',
+              textDecoration: 'underline',
+              fontSize: 16,
+            }}
+            onClick={() => navigate('/register')}
+          >
+            Sign up here
           </Typography>
-          <Button type="submit" variant="contained" size="large">
-            Log In
-          </Button>
-        </Box>
+        </Typography>
+        <Button type="submit" variant="contained" size="large">
+          Log In
+        </Button>
       </Box>
     </Box>
   );
