d3657de93082a8d46c8ec2154632530d006f5d47
Alex Xu <xulex6@gmail.com>
2023-09-02 12:19:02 +0000

    feat: update bid ask submission for multiple markets

--- frontend/src/components/BidAskPanel.jsx
+++ frontend/src/components/BidAskPanel.jsx
@@ -12,7 +12,16 @@ import GroupIcon from '@mui/icons-material/Group';
 import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
 import RequestPageOutlinedIcon from '@mui/icons-material/RequestPageOutlined';
 
-const BidAskPanel = ({ teamId, teamName, balance, contracts, position }) => {
+const BidAskPanel = ({
+  teamId,
+  teamName,
+  balance,
+  contracts,
+  position,
+  marketIndex,
+  lastBid,
+  lastAsk,
+}) => {
   const [isTradeSuccess, setIsTradeSuccess] = useState(false);
   const [bid, setBid] = useState('');
   const [ask, setAsk] = useState('');
@@ -25,9 +34,10 @@ const BidAskPanel = ({ teamId, teamName, balance, contracts, position }) => {
     const res = await fetchAPIRequest(`/game/${teamId}/submit`, 'PUT', {
       bid,
       ask,
+      marketIndex,
     });
 
-    res.status === 200 && setIsTradeSuccess(true);
+    // res.status === 200 && setIsTradeSuccess(true);
   };
 
   return (
@@ -91,7 +101,8 @@ const BidAskPanel = ({ teamId, teamName, balance, contracts, position }) => {
           sx={{ mr: 1 }}
           autoFocus
           onChange={(event) => setBid(event.target.value)}
-          value={bid}
+          disabled={lastBid !== 0 || lastAsk !== 0}
+          value={lastBid ? lastBid : bid}
         />
         <TextField
           color="success"
@@ -99,102 +110,19 @@ const BidAskPanel = ({ teamId, teamName, balance, contracts, position }) => {
           type="number"
           sx={{ ml: 1 }}
           onChange={(event) => setAsk(event.target.value)}
-          value={ask}
+          disabled={lastBid !== 0 || lastAsk !== 0}
+          value={lastAsk ? lastAsk : ask}
         />
       </Box>
       <Button
         variant="contained"
-        disabled={isTradeSuccess}
+        disabled={lastBid !== 0 || lastAsk !== 0}
         onClick={submitSpread}
         sx={{ mx: 'auto', px: 5, mt: 2 }}
       >
         Submit
       </Button>
     </Box>
-    // <Box
-    //   sx={{
-    //     backgroundColor: 'white',
-    //     boxShadow: 3,
-    //     borderRadius: 3,
-    //     p: 3,
-    //     display: 'flex',
-    //     flexDirection: 'column',
-    //     flex: '1 1 auto',
-    //     width: { xs: '80%', md: '70%' },
-    //     maxWidth: 500,
-    //     m: 'auto',
-    //     mb: 3,
-    //   }}
-    // >
-    //   <Typography
-    //     variant="h5"
-    //     sx={{
-    //       // width: { xs: 2, sm: 100, md: 150, lg: 170 },
-    //       // textOverflow: 'ellipsis',
-    //       // overflow: 'hidden',
-    //       // whiteSpace: 'nowrap',
-    //       wordBreak: 'break-all',
-    //       display: 'flex',
-    //       alignItems: 'center',
-    //     }}
-    //   >
-    //     <GroupIcon fontSize="large" sx={{ mr: 1 }} />
-    //     {teamName}
-    //   </Typography>
-    //   {isTradeSuccess && (
-    //     <Typography color="text.secondary">Awaiting next round</Typography>
-    //   )}
-    //   {isTradeSuccess && <LinearProgress />}
-    //   <Divider sx={{ my: 2 }} />
-    //   <Box sx={{ display: 'flex', justifyContent: 'space-around', mb: 2 }}>
-    //     <Typography variant="h6" sx={{ display: 'flex', alignItems: 'center' }}>
-    //       <PaidTwoToneIcon fontSize="large" sx={{ mr: 1 }} />
-    //       {balance}
-    //     </Typography>
-    //     <Typography variant="h6" sx={{ display: 'flex', alignItems: 'center' }}>
-    //       <RequestPageTwoToneIcon
-    //         fontSize="large"
-    //         color="primary"
-    //         sx={{ mr: 1 }}
-    //       />{' '}
-    //       {contracts} contracts
-    //     </Typography>
-    //   </Box>
-    //   <Box sx={{ display: 'flex', justifyContent: 'space-evenly', mb: 2 }}>
-    //     <TextField
-    //       color="error"
-    //       label="Bid Price"
-    //       type="number"
-    //       inputProps={{
-    //         style: { fontSize: 20, fontFamily: 'monospace' },
-    //       }}
-    //       sx={{ mr: 1 }}
-    //       autoFocus
-    //       onChange={(event) => setBid(event.target.value)}
-    //       value={bid}
-    //     />
-    //     <TextField
-    //       color="success"
-    //       label="Ask Price"
-    //       type="number"
-    //       inputProps={{
-    //         style: { fontSize: 20, fontFamily: 'monospace' },
-    //       }}
-    //       sx={{ ml: 1 }}
-    //       onChange={(event) => setAsk(event.target.value)}
-    //       value={ask}
-    //     />
-    //   </Box>
-    //   <Button
-    //     size="large"
-    //     variant="contained"
-    //     disabled={isTradeSuccess}
-    //     onClick={submitSpread}
-    //     sx={{ mx: 4 }}
-    //   >
-    //     Submit
-    //   </Button>
-    // </Box>
   );
 };
 export default BidAskPanel;
