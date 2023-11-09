e4a5e2518169e6b2f53adf1eff1c60f7413601d1
Shmu9 <sam.axford02@gmail.com>
2023-11-10 03:01:05 +1100

    feat: add DigitalOcean Droplet backend IP.

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,4 @@
 export const BACKEND_PORT = 5005;
-export const BACKEND_ROUTE = 'http://localhost:5005';
+// export const BACKEND_ROUTE = 'http://localhost:5005';
+export const BACKEND_ROUTE = '//170.64.208.236';
+// export const BACKEND_ROUTE = '//' + process.env.DIGITALOCEAN_DROPLET_SSH_HOST;
