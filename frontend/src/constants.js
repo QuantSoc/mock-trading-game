2c285d47bebd1aa633b8a6aead6d6be243008424
Shmu9 <sam.axford02@gmail.com>
2023-11-10 23:22:41 +1100

    test: SSL setup for backmtg.quantsoc.org

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
 export const BACKEND_PORT = 5005;
 // export const BACKEND_ROUTE = 'http://localhost:5005';
-export const BACKEND_ROUTE = 'http://170.64.208.236';
+export const BACKEND_ROUTE = 'https://backmtg.quantsoc.org';
 // export const BACKEND_ROUTE = '//' + process.env.DIGITALOCEAN_DROPLET_SSH_HOST;
